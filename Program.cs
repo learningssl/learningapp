36148501a6304cbebce7186b7dae7206e200c406
learningssl <seetha.lakshmissl@outlook.com>
2025-02-03 23:14:51 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World! version 1 ");
+app.MapGet("/", () => "Hello World! Version 1 ");
 
 app.Run();
